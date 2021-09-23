f7b1d795b99bc1be86dccce6dee6b8cbd453154b
seungjuoh <sjoh0704@daum.net>
2021-09-23 09:01:39 +0000

    just save

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 echo "-----------------------------------------------------------------------"
 echo "configure envrionment setting"
-export CART_SERVICE_URL=http://172.30.1.35:8080 # local ip
-export RATING_SERVICE_URL=http://172.30.1.35:8081 # local ip
+local_host=172.30.1.50
+
+export CART_SERVICE_URL=http://$local_host:8080 # local ip
+export RATING_SERVICE_URL=http://$local_host:8081 # local ip
 # export CART_SERVICE_URL=http://192.168.219.171:8080 # local ip
 # export RATING_SERVICE_URL=http://192.168.219.171:8081 # local ip
 
-export MYSQL_PRODUCT_HOST=172.17.0.4 
-export MYSQL_ORDER_HOST=172.17.0.4
-export MYSQL_USER_HOST=172.17.0.4
+export MYSQL_PRODUCT_HOST=172.17.0.3 
+export MYSQL_ORDER_HOST=172.17.0.3
+export MYSQL_USER_HOST=172.17.0.3
 echo "-----------------------------------------------------------------------"
 echo "migrate and activate server!"
-
-python3 ./apigateway/manage.py runserver 0.0.0.0:8000 &
 
 python3 ./product/manage.py migrate
 python3 ./product/manage.py runserver 0.0.0.0:8100 &
