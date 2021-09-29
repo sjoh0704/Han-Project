202e4fb7059b6491bf2efa0bb619f558dffd40a7
seungjuoh <sjoh0704@daum.net>
2021-09-29 23:05:03 +0900

    update loadtester

--- loadgenerator/load.py
+++ loadgenerator/load.py
@@ -4,37 +4,54 @@ from time import sleep
 FRONTEND = os.getenv("FRONTEND", "localhost:3000")
 
 def gernerateLoad(target):
+    print("--------------------------------------------------------")
     print("GET {}".format(target))
     cmd = 'curl --max-time 5 {}'.format(target)
     res = os.popen(cmd).read()
     print(res)
-    sleep(0.3)
+    sleep(0.1)
 
 def userPlayBook(frontend):
+    print("--------------------------------------------------------")
     print("user create and delete playbook")
     print("create user")
-    username = os.popen("cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q").read()
-   
-    data = {
-        "username": username,
-        "password": "1234",
-        "email": "abcd@abcd.com",
-        "phone_number": "01011111111"
-    }
-    cmd = "curl --max-time 5 -d '{}'\
+    username = os.popen("cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q").read().strip()
+    data = '{ \
+    "username": "%s", \
+    "password": "1234", \
+    "email": "abcd@abcd.com", \
+    "phone_number": "01011111111" \
+    }' %(username)
+    cmd = "curl --max-time 5 -d \'{}\'\
     -H 'Content-Type: application/json' \
-    -X POST {}/apis/v1/user | jq .payload.user_id".format(data, "172.17.0.2:8200")
-    print(cmd)
-    # os.system(cmd)
+    -X POST {}/apis/v1/user/ | jq '.payload.user_id'".format(data, FRONTEND)
     user_id = os.popen(cmd).read()
-    print(user_id)
 
-userPlayBook(FRONTEND)
 
-# gernerateLoad(FRONTEND + "/apis/v1/user/1")
-# gernerateLoad(FRONTEND + "/apis/v1/product/0")
-# gernerateLoad(FRONTEND + "/apis/v1/carts/users/0")
-# gernerateLoad(FRONTEND + "/apis/v1/ratings/0")
-# gernerateLoad(FRONTEND + "/apis/v1/order/0")
-# gernerateLoad(FRONTEND + "/apis/v1/store/0")
-# gernerateLoad(FRONTEND + "/apis/v1/post/0")
+    if not username:
+        print("Error: username doesn't exist")
+        return
+    
+    gernerateLoad(FRONTEND + "/apis/v1/ratings/{}".format(user_id))
+    
+    
+    print("\ndelete user")
+    
+    delete_cmd = "curl --max-time 5 \
+    -H 'Content-Type: application/json' \
+    -X DELETE {}/apis/v1/user/{}".format(frontend, user_id)
+    os.system(delete_cmd)
+    sleep(0.5)
+
+
+print("load generator starts")
+while 1:
+    print("load generate!")
+    gernerateLoad(FRONTEND + "/apis/v1/product/1")
+    gernerateLoad(FRONTEND + "/apis/v1/carts/users/0")
+    gernerateLoad(FRONTEND + "/apis/v1/order/0")
+    gernerateLoad(FRONTEND + "/apis/v1/store/1")
+    gernerateLoad(FRONTEND + "/apis/v1/post/1")
+    userPlayBook(FRONTEND)
+    print("\n")
+print("load generator stop")
